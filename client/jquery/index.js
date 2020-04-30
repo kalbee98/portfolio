f27bd97ab3c28d99bffc4e76f9a7a193c9cd8012
kalbee98 <test@example.com>
2020-05-01 00:15:54 +0900

    なんとなく要素を配置

--- client/jquery/index.js
+++ client/jquery/index.js
@@ -10,6 +10,10 @@ function Todo($target){
 		id: 2,
 		task: 'javascript',
 		limit: 'tomorrow'
+	},{
+		id: 3,
+		task: '<b>xss</b>',
+		limit: '2020/5/2'
 	}];
 	this.$target = $target;
 }
@@ -19,7 +23,15 @@ Todo.prototype = {
 		this.show();
 	},
 	show: function(){
-		console.log(this.$target.find('#todos'));
+		var str = '';
+		$.each(this.todos, function(i, n){
+			str = str + '<div class="row">';
+			str = str + '<div><input type="checkbox" /></div>';
+			str = str + '<div class="cell">' + escapeHtml(n.task) + '</div>';
+			str = str + '<div class="cell">' + escapeHtml(n.limit) + '</div>';
+			str = str + '</div>';
+		});
+		$('#todos', this.$target).html(str);
 	},
 	add: function(){
 		console.log('add');
@@ -33,4 +45,15 @@ Todo.prototype = {
 	search: function(){
 		console.log('search');
 	}
+}
+
+
+//utilities
+function escapeHtml(str) {
+  str = str.replace(/&/g, '&amp;');
+  str = str.replace(/</g, '&lt;');
+  str = str.replace(/>/g, '&gt;');
+  str = str.replace(/"/g, '&quot;');
+  str = str.replace(/'/g, '&#39;');
+  return str;
 }
