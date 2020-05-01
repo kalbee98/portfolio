618025b82fec119bdb8677a6c5c5500e9b533592
kalbee98 <test@example.com>
2020-05-01 11:51:12 +0900

    画面デザイン完成

--- client/jquery/index.js
+++ client/jquery/index.js
@@ -14,6 +14,9 @@ function Todo($target){
 		id: 3,
 		task: '<b>xss</b>',
 		limit: '2020/5/2'
+	}, {
+		id: 5,
+		task: 'ほげふがぴよ'
 	}];
 	this.$target = $target;
 }
@@ -24,13 +27,15 @@ Todo.prototype = {
 	},
 	show: function(){
 		var str = '';
+		str = str + '<div class="table">'
 		$.each(this.todos, function(i, n){
 			str = str + '<div class="row">';
-			str = str + '<div><input type="checkbox" /></div>';
-			str = str + '<div class="cell">' + escapeHtml(n.task) + '</div>';
-			str = str + '<div class="cell">' + escapeHtml(n.limit) + '</div>';
+			str = str + '<div class="cell check"><input type="checkbox" /></div>';
+			str = str + '<div class="cell task">' + escapeHtml(n.task) + '</div>';
+			str = str + '<div class="cell limit">' + escapeHtml(n.limit) + '</div>';
 			str = str + '</div>';
 		});
+		str = str + '</div>'
 		$('#todos', this.$target).html(str);
 	},
 	add: function(){
@@ -47,13 +52,13 @@ Todo.prototype = {
 	}
 }
 
-
 //utilities
 function escapeHtml(str) {
-  str = str.replace(/&/g, '&amp;');
-  str = str.replace(/</g, '&lt;');
-  str = str.replace(/>/g, '&gt;');
-  str = str.replace(/"/g, '&quot;');
-  str = str.replace(/'/g, '&#39;');
-  return str;
+	str = str || '';
+	str = str.replace(/&/g, '&amp;');
+	str = str.replace(/</g, '&lt;');
+	str = str.replace(/>/g, '&gt;');
+	str = str.replace(/"/g, '&quot;');
+	str = str.replace(/'/g, '&#39;');
+	return str;
 }
