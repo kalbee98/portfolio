de35ad58621d197115b8310f0f0a54d57d1fef13
kalbee98 <test@example.com>
2020-05-04 21:26:26 +0900

    カウンターコンポーネントを追加する

--- client/vuejs/src/components/App.vue
+++ client/vuejs/src/components/App.vue
@@ -1,11 +1,20 @@
+<script>
+import Counter from "./Counter";
+
+export default {
+  components: { Counter }
+};
+</script>
+
 <template>
   <div>
     <p>Hello, World!</p>
+    <counter/>
   </div>
 </template>
 
 <style scoped>
-  * {
-    border: 1px solid red;
-  }
+* {
+  border: 1px solid red;
+}
 </style>
