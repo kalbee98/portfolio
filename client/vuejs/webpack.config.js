b4ac277cba9e6f510ff3713c3923b26545cb5d10
kalbee98 <test@example.com>
2020-05-04 21:03:02 +0900

    Vueのインストール

--- client/vuejs/webpack.config.js
+++ client/vuejs/webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path')
 
+// vue-loader@15から必要
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
 module.exports = {
     // エントリポイントのファイル
     entry: './src/index.js',
@@ -9,6 +12,31 @@ module.exports = {
         // 出力ファイル名
         filename: 'bundle.js'
     },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/, // ファイルが.vueで終われば...
+                loader: 'vue-loader' // vue-loaderを使う
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.css$/,
+                use: ['vue-style-loader', 'css-loader'] // css-loader -> vue-style-loaderの順で通していく
+            },
+        ]
+    },
+    resolve: {
+        // import './foo.vue' の代わりに import './foo' と書けるようになる(拡張子省略)
+        extensions: ['.js', '.vue'],
+        alias: {
+            // vue-template-compilerに読ませてコンパイルするために必要
+            vue$: 'vue/dist/vue.esm.js',
+        },
+    },
+    plugins: [VueLoaderPlugin()],
     devServer: {
         // webpackの扱わないファイル(HTMLや画像など)が入っているディレクトリ
         contentBase: path.resolve(__dirname, 'public')
